2829b97556b2c89dff095e190f703897c3c03991
Yannick Hebert <yannickltc@gmail.com>
2024-12-11 09:04:12 +0100

    Ajout des fonctionnalités de vue et d'édition avec le formulaire

--- script.js
+++ script.js
@@ -109,9 +109,14 @@ function editModal(gameId) {
 	// console.log(gameId, gamesList)
 	// Trouvez le jeu en fonction de son identifiant
 	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
-	modifyModal("Mode Edition")
+	fetch("./form.html").then((data) => {
+		data.text().then((form) => {
+			modifyModal("Mode Edition", form)
+		})
+	})
+	// passer une image comme corps du modal
+	// const modalBody = `<h4>ajoutez un formulaire pour modifier le jeu ici</h4>`
 }
-
 function modifyModal(modalTitle) {
 	// Écrir le nom du jeu dans le titre du modal
 	document.querySelector(".modal-title").textContent = gamesList[result].title
@@ -137,9 +142,13 @@ function editModal(gameId) {
 	// console.log(gameId, gamesList)
 	// Trouvez le jeu en fonction de son identifiant
 	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	fetch("./form.html").then((data) => {
+		data.text().then((form) => {
+			modifyModal("Mode Edition", form)
+		})
+	})
 	// passer une image comme corps du modal
-	const modalBody = `<h4>ajoutez un formulaire pour modifier le jeu ici</h4>`
-	modifyModal("Mode Edition", modalBody)
+	// const modalBody = `<h4>ajoutez un formulaire pour modifier le jeu ici</h4>`
 }
 
 function modifyModal(modalTitle, modalBody) {
@@ -147,5 +156,7 @@ function modifyModal(modalTitle, modalBody) {
 	document.querySelector(".modal-title").textContent = modalTitle
 	document.querySelector(".modal-body").innerHTML = modalBody
 }
+
+
 
 
