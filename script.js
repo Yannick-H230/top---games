f7a6b269e710e9f6e33474c534000ea08e6c91f5
Yannick Hebert <yannickltc@gmail.com>
2024-12-19 16:52:49 +0100

    Initial commit - ajout du projet Bootstrap

--- script.js
+++ script.js
@@ -1,78 +1,58 @@
+// initialize variable
+let carsList
+
+fetch("http://localhost:3000/api/cars", {
+	method: "GET",
+	headers: {
+		"x-api-key": "secret_phrase_here",
+		"Content-Type": "application/json",
+		Accept: "application/json",
+	},
+})
+	.then((res) => {
+		if (!res.ok) {
+			console.log("your API isn't working !!!")
+		}
+		res.json().then((data) => {
+			console.log(data)
+			carsList = data // Mise à jour de la liste des voitures avec les données récupérées
+			writeDom()  // APRÈS que les données aient été récupérées 
+			editButtons = document.querySelectorAll(".edit")
+			editButtons.forEach((btn) => {
+				btn.addEventListener("click", (e) => {
+					editModal(e.target.getAttribute("data-edit-id"))
+				})
+			})
+
+			viewButtons = document.querySelectorAll(".view")
+			viewButtons.forEach((btn) => {	
+				btn.addEventListener("click", (e) => {
+					viewModal(e.target.getAttribute("data-edit-id"))
+				})
+			})
+		})
+	})
+	.catch((error) =>
+		console.error("Erreur lors de la récupération des voitures :", error)
+	)
 
 
-const gamesList = [
-	{
-		title: "Tekken",
-		year: 1994,
-		imageUrl:
-			"https://cdn.dashfight.com/bcf6a9046a9ea4c1070d4aedb2981103c978a704.png",
-		id: 1,
-	},
-	{
-		title: "Minecraft",
-		year: 2009,
-		imageUrl:
-			"https://m.media-amazon.com/images/I/61smNbXSW1L._AC_UF1000,1000_QL80_.jpg",
-		id: 2,
-	},
-	{
-		title: "Elden Ring",
-		year: 2022,
-		imageUrl:
-			"https://pic.clubic.com/v1/images/1934271/raw?fit=smartCrop&width=1200&height=675&hash=e7519a9577a2b7291fa26880bb22bed6740836be",
-		id: 3,
-	},
-	{
-		title: "Street Fighter V",
-		year: 2015,
-		imageUrl:
-			"https://gaming-cdn.com/images/products/671/orig/street-fighter-v-pc-jeu-steam-cover.jpg?v=1662539255",
-		id: 4,
-	},
-	{
-		title: "Half Life 2",
-		year: 2004,
-		imageUrl:
-			"https://gaming-cdn.com/images/products/2284/orig/half-life-2-pc-mac-game-steam-cover.jpg?v=1650555068",
-		id: 5,
-	},
-	{
-		title: "Skyrim",
-		year: 2011,
-		imageUrl:
-			"https://gaming-cdn.com/images/products/146/orig/the-elder-scrolls-v-skyrim-pc-jeu-steam-europe-cover.jpg?v=1661270991",
-		id: 6,
-	},
-    {
-		title: "* BRAWL STAR *",
-		year: 2018,
-		imageUrl:
-			"https://supercell.com/images/e93a34598d3723641a72eb9ce02691f6/790/games_thumbnail_brawlstars.5cd76330.webp",
-		id: 7,
-	},	{
-		title: "Pokemon",
-		year: 1995,
-		imageUrl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTERSnkLVBs2FhceOgJUJ8LscTH5OUWwzxSg&s",
-		id: 8,
-	}
-]
 
 function writeDom() {
-	gamesList.forEach((game) => {
+	carsList.forEach((car) => {
 		const articleContainer = document.querySelector(".row")
 		articleContainer.innerHTML += `<article class="col">
 <div class="card shadow-sm">
 
-    <img src="${game.imageUrl}" alt="${game.title}" class="card-img-top" />
+    <img src="${car.carImage}" alt="${car.carName}" class="card-img-top" />
 
     <div class="card-body">
 
-    <h3 class="card-title">${game.title}</h3>
+    <h3 class="card-title">${car.carName}</h3>
 
         <p class="card-text">
 
-        ${game.title}
+        ${car.carYear}
 
         </p>
         
@@ -80,12 +60,12 @@ function writeDom() {
             <div class="btn-group">
                 <button type="button" class="btn btn-sm btn-outline-secondary view "
                 data-bs-toggle="modal" data-bs-target="#exampleModal"
-                data-edit-id="${game.id}"
+                data-edit-id="${car.id}"
                 >View
             </button>
                 <button type="button" class="btn btn-sm btn-outline-secondary edit"
                 data-bs-toggle="modal" data-bs-target="#exampleModal"
-                data-edit-id="${game.id}"
+                data-edit-id="${car.id}"
                 >Edit
             </button>
             </div>
@@ -95,94 +75,105 @@ function writeDom() {
 </article>`
 	})
 }
-writeDom()
 
 
-editButtons = document.querySelectorAll(".edit")
-editButtons.forEach((btn) => {
-	btn.addEventListener("click", (e) => {
-		editModal(e.target.getAttribute("data-edit-id"))
-	})
-})
 
-function editModal(gameId) {
-	// Trouvez le jeu en fonction de son identifiant
-	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
-	// Injectez le formulaire dans le corps du modal
-	fetch("./form.html").then((data) => {
-		data.text().then((form) => {
-			// Modifiez le titre et le corps du modal
-			const selectedGame = gamesList[result]
-			modifyModal("Mode Edition", form)
-			modifyFom({
-				title: selectedGame.title,
-				year: selectedGame.year,
-				imageUrl: selectedGame.imageUrl,
-			})
-			document
-			.querySelector('button[type="submit"]')
-			.addEventListener("click", updateGames)
-		})
-	})
-}
 
-function modifyFom(gameData) {
+function modifyFom(carData) {
 	const form = document.querySelector("form")
-	form.title.value = gameData.title
-	form.year.value = gameData.year
-	form.imageUrl.value = gameData.imageUrl
+	form.title.value = carData.title
+	form.year.value = carData.year
+	form.imageUrl.value = carData.imageUrl
 }
 
 function modifyModal(modalTitle) {
 	// Écrir le nom du jeu dans le titre du modal
-	document.querySelector(".modal-title").textContent = gamesList[result].title
+	document.querySelector(".modal-title").textContent = carsList[result].title
 }
 
-viewButtons = document.querySelectorAll(".view")
-viewButtons.forEach((btn) => {
-	btn.addEventListener("click", (e) => {
-		viewModal(e.target.getAttribute("data-edit-id"))
-	})
-})
+
 
 function viewModal(gameId) {
-	// console.log(gameId, gamesList)
 	// Trouvez le jeu en fonction de son identifiant
-	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
-	// passer une image comme corps du modal
-	const modalBody = `<img src="${gamesList[result].imageUrl}" alt="${gamesList[result].title}" class="img-fluid" />`
-	modifyModal(gamesList[result].title, modalBody)
-	// edit footer
-	// Écrire dans le footer
-	document.querySelector(".modal-footer").innerHTML = `
+	fetch(`http://localhost:3000/api/cars/${gameId}`, {
+		method: "GET",
+		headers: {
+			"x-api-key": "secret_phrase_here",
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+	})
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error("Error with the car with this id")
+			}
+			res.json().then((data) => {
+				console.log(data)
+				const selectedCar = data
+				// passer une image comme corps du modal
+				const modalBody = `<img src="${selectedCar.carImage}" alt="${selectedCar.carName}" class="img-fluid" />`
+				modifyModal(selectedCar.carName, modalBody)
+				// edit footer
+				// Écrire dans le footer
+				document.querySelector(".modal-footer").innerHTML = `
 		<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
 			Close
 		</button>
 </form>`
+			})
+		})
+		.catch((error) =>
+			console.error("Erreur lors de la récupération des voitures :", error)
+		)
 }
 
-function editModal(gameId) {
+function editModal(carId) {
 	// Trouvez le jeu en fonction de son identifiant
-	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
-	// Injectez le formulaire dans le corps du modal
-	fetch("./form.html").then((data) => {
-		data.text().then((form) => {
-			// Modifiez le titre et le corps du modal
-			const selectedGame = gamesList[result]
-			modifyModal("Mode Edition", form)
-			modifyFom({
-				title: selectedGame.title,
-				year: selectedGame.year,
-				imageUrl: selectedGame.imageUrl,
-			})
-			document
-			.querySelector('button[type="submit"]')
-			.addEventListener("click", () =>
-				updateGames(title.value, year.value, imageUrl.value, gameId)
-			)
-		})
+	console.log(carId)
+	// fetch car by ID // http://localhost:3000/api/cars/1
+	fetch(`http://localhost:3000/api/cars/${carId}`, {
+		method: "GET",
+		headers: {
+			"x-api-key": "secret_phrase_here",
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
 	})
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error("Error with the car with this id")
+			}
+			res.json().then((data) => {
+				console.log(data)
+				const selectedCar = data
+
+				// Injectez le formulaire dans le corps du modal
+				fetch("./form.html").then((data) => {
+					
+
+					data.text().then((form) => {
+						// Modifiez le titre et le corps du modal
+						modifyModal("Mode Edition", form)
+						modifyFom({
+							title: selectedCar.carName,
+							year: selectedCar.carYear,
+							imageUrl: selectedCar.carImage,
+						})
+						
+						console.log(document.querySelector('button[type="submit"]'))
+						document.querySelector('button[type="submit"]').addEventListener("click", () => 
+								updateGames(title.value, year.value, imageUrl.value, carId)
+															
+							)
+					})
+				})
+			})
+		})
+		.catch((error) =>
+			console.error("Erreur lors de la récupération des voitures :", error)
+		)
 }
+
 
 function modifyModal(modalTitle, modalBody) {
 	// Écrire le nom du jeu dans le titre du modal
@@ -198,27 +189,21 @@ function modifyModal(modalTitle, modalBody) {
 </form>`
 }
 
-function updateGames(title, year, imageUrl, gameId) {
-	// Trouvez le jeu en fonction de son identifiant
-	const index = gamesList.findIndex((game) => game.id === parseInt(gameId))
-
-	gamesList[index].title = title
-	gamesList[index].year = year
-	gamesList[index].imageUrl = imageUrl
-	document.querySelector(".row").innerHTML = "" // Nous supprimons toutes les données des jeux dans le DOM.
-	writeDom()
-	editButtons = document.querySelectorAll(".edit")
-	editButtons.forEach((btn) => {
-		btn.addEventListener("click", (e) => {
-			editModal(e.target.getAttribute("data-edit-id"))
-		})
-	})
+function updateGames(title, year, imageUrl, carId) {
 	
-	viewButtons = document.querySelectorAll(".view")
-	viewButtons.forEach((btn) => {
-		btn.addEventListener("click", (e) => {
-			viewModal(e.target.getAttribute("data-edit-id"))
-		})
-	})
+const formdata = {
+	"carName" : title,
+	"carYear" : year,
+	"imageUrl" : imageUrl
 }
 
+fetch(`http://localhost:3000/api/cars/${carId}`, {
+	method: "PUT",
+	headers: {
+		"x-api-key": "secret_phrase_here",
+		"Content-Type": "application/json",
+		Accept: "application/json",
+	},
+	body : JSON.stringify(formdata)
+})
+}
